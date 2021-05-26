624852eaa89548b2e57101f53b3444716b55ad13
Martin Mählmann <maehlmann@protonmail.com>
2021-05-26 20:43:54 +0200

    made mockgen.sh sh compatible

--- mockgen.sh
+++ mockgen.sh
@@ -1,26 +1,23 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
+GO_FILES=$(find . -type f -name "*.go" -not -path "./vendor/*")
 
-# list all go files in project, vendor directory included
-for dir in **/*.go; do
-  # filter for files except in vendor directory
-  if ! [[ "$dir" =~ ^vendor* ]]; then
-    # check if file contains one or more interfaces
-    if grep -q "type.*interface\s{" "$dir"; then
-      # get the path without the file
-      path_without_file="$(dirname "$dir")"
+for dir in $GO_FILES; do
+  # check if file contains one or more interfaces
+  if grep -q "type.*interface\s{" "$dir"; then
+    # get the path without the file
+    path_without_file="$(dirname "$dir")"
 
-      # get the filename without the path and without extension
-      filename="$(basename "$dir" .go)"
+    # get the filename without the path and without extension
+    filename="$(basename "$dir" .go)"
 
-      # extract the package name
-      package="$(basename "$path_without_file")"
+    # extract the package name
+    package="$(basename "$path_without_file")"
 
-      # create the mock directory, if it does not already exist
-      mkdir -p "$path_without_file/mock/"
+    # create the mock directory, if it does not already exist
+    mkdir -p "$path_without_file/mock/"
 
-      # run mockgen on all identified files
-      echo "mockgen -source=$dir -destination=$path_without_file/mock/$filename\_mock.go -package=$package\_mock"
-      mockgen -source="$dir" -destination="$path_without_file"/mock/"$filename"\_mock.go -package="$package"_mock
-    fi
+    # run mockgen on all identified files
+    echo "mockgen -source=$dir -destination=$path_without_file/mock/$filename\_mock.go -package=$package\_mock"
+    mockgen -source="$dir" -destination="$path_without_file"/mock/"$filename"\_mock.go -package="$package"_mock
   fi
 done
